6c255b7ee41d03cacb25fb80b0b2c2b918ce155e
Ömer DİRİCANLI <omrdrcnl@gmail.com>
2023-09-02 16:04:22 +0300

    OrderApp

---
+++ BasicApps/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace BasicApps
+{
+    class program
+    {
+        static double kara(double x, double y)
+        {
+            return x + y;
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("Lütfen toplamak istediğiniz sayıları arasında birer boşluk olacak şekilde 2 sayı giriniz");
+
+            string[] sayilar = Console.ReadLine().Split(" ");
+
+            if (sayilar.Length != 2)
+            {
+                Console.WriteLine("Lütfen geçerli bir format giriniz");
+            }
+            else
+            {
+
+                double.TryParse(sayilar[0], out double sayi1);
+                double.TryParse(sayilar[1], out double sayi2);
+
+                double sonuc = kara(sayi1, sayi2);
+
+                Console.WriteLine("sonuç: " + sonuc);
+            }
+
+
+        }
+    }
+}
---
+++ OrderApp/Program.cs
@@ -0,0 +1,123 @@
+﻿using System.Threading.Channels;
+
+namespace OrderApp
+{
+    class program
+    {
+        static List<Order> orders = new List<Order>();
+
+        static void Main(string[] Args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Lütfen Seçim Yapınız.");
+                Console.WriteLine("1. Görev Ekle");
+                Console.WriteLine("2. Görev Listele");
+                Console.WriteLine("3. Görev Tamamla");
+                Console.WriteLine("4. Görev Sil");
+                Console.WriteLine("5. Çıkış yap");
+
+                string islem = Console.ReadLine();
+                switch (islem)
+                {
+                    case "1":
+                        AddOrder(); break;
+                    case "2":
+                        OrderList(); break;
+                    case "3":
+                        OrderDone(); break;
+                    case "4":
+                        OrderDelete(); break;
+                    case "5":
+                        Console.WriteLine("Çıkış yapılıyor...");
+                        return;
+                    default:
+                        Console.WriteLine("Lütfen geçerli bir değer giriniz");
+                        break;
+
+                }
+
+            }
+        }
+
+        static void AddOrder()
+        {
+            Console.WriteLine("Görev ekleyiniz");
+
+            string girdi = Console.ReadLine();
+
+            if (!string.IsNullOrEmpty(girdi))
+            {
+                string ord = girdi;
+                Order order = new Order(ord);
+                orders.Add(order);
+                Console.WriteLine("Görev başarıyla eklendi");
+            }
+            else
+            {
+                Console.WriteLine("Lütfen string bir değer giriniz.");
+            }
+
+        }
+        static void OrderList()
+        {
+            Console.WriteLine("----------");
+            for (int i = 0; i < orders.Count(); i++)
+            {
+                Console.WriteLine((i + 1) + ". " + orders[i].Name + " Yapıldı mı ?" + (orders[i].isDone == false ? "Hayır" : "Evet"));
+            }
+        }
+        static void OrderDone()
+        {
+            OrderList();
+            Console.WriteLine("Lütfen tamamladığınız görevi seçiniz:");
+
+            string girdi = Console.ReadLine();
+
+            if (girdi != null && Convert.ToInt32(girdi) <= orders.Count())
+            {
+                int o = Convert.ToInt32(girdi);
+
+                orders[o - 1].isDone = true;
+            }
+            else
+            {
+                Console.WriteLine("Lütfen doğru bir değer giriniz.");
+            }
+
+        }
+
+        static void OrderDelete()
+        {
+            OrderList();
+            Console.WriteLine("Lütfen silmek istediğiniz görevi seçiniz:");
+
+            string girdi = Console.ReadLine();
+
+
+            if (girdi != null && Convert.ToInt32(girdi) < orders.Count())
+            {
+                int o = Convert.ToInt32(girdi);
+
+                orders.RemoveAt(o-1);
+            }
+            else
+            {
+                Console.WriteLine("Lütfen doğru bir değer giriniz.");
+            }
+        }
+
+    }
+
+    class Order
+    {
+        public Order(string name)
+        {
+            Name = name;
+            isDone = false;
+        }
+
+        public string Name { get; set; }
+        public bool isDone { get; set; }
+    }
+}
