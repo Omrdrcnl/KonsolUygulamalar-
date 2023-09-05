b02061fb5bdddcd39c273fe674bc5ba34d36c43f
Ömer DİRİCANLI <omrdrcnl@gmail.com>
2023-09-05 18:54:43 +0300

    CappEducation

---
+++ CappEducation/Program.cs
@@ -0,0 +1,26 @@
+﻿// Local fonksiyonlar-- C# 7 ile birlikte gelmiştir.
+
+//
+namespace CappEducation
+{
+    class CappEducation
+    {
+        static void Main(string[] args)
+        {
+            x();
+        }
+        //Local fonksiyonlar yalnızca tanımlandıgı method içerisinden erişilebilir.
+        // Üst fonksiyonla aynı isimde tanımlanırsa içerisinde ki fonksiyon dısardakini ezecektir.
+        public static void x()
+        {
+            void y()
+            {
+                Console.WriteLine("X fonksiyonu içerisinde ki Y fonksiyonu tetiklendi.");
+            }
+
+            y();
+        }
+        //İleri seviyede Anonim, Delegate, Func gibi yapılar local fuunction yerine kullanılıyor.
+
+    }
+}
